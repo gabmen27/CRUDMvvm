fb34ab87decb78328e75b8d52d29b8e8acc00fe1
gabmen27 <gabrieljesus680@gmail.com>
2025-03-14 23:57:42 -0600

    Creaccion de la base de datos

---
+++ CRUDMvvm/Services/ProveedorService.cs
@@ -0,0 +1,14 @@
+﻿
+using SQLite;
+
+namespace CRUDMvvm.Services
+{
+    class ProveedorService
+    {
+        private readonly SQLiteConnection _connection;
+
+        public ProveedorService() {
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"Proveedor.db3");
+        }
+    }
+}
